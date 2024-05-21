1810fe528e44f2a49c5535827ad85fc102a7ce2e
Davide D'Antonio <9164563+davidedantonio@users.noreply.github.com>
2024-05-21 12:51:17 +0200

    enh

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.js",
-  unstable_flexsearch: true,
-  unstable_staticImage: true,
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.js",
+	unstable_flexsearch: true,
+	unstable_staticImage: false,
 });
 
 module.exports = withNextra({
-  experiments: {
-    esmExternals: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/data-analyst',
-        permanent: true,
-      },
-    ]
-  },
+	experiments: {
+		esmExternals: true,
+	},
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/data-analyst",
+				permanent: true,
+			},
+		];
+	},
 });
